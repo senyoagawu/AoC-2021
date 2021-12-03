aaa445cd7d28358f2bea91cad304cd26b6ea6629
sdkag <senyo.k.agawu@gmail.com>
2021-12-03 13:19:24 -0500

    reorganize

--- 1/1.py
+++
@@ -1,7 +0,0 @@
-with open('1.txt') as f:
-    lines = f.readlines()
-    count = 0
-    for idx, l in enumerate(lines):
-        if idx != 0 and int(l) > int(lines[idx-1]):
-            count += 1
-    print(count)
